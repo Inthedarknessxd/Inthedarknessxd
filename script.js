9b77b002922cff432ac0ef293b69ca112230cef6
Inthedarknessxd <108297974+Inthedarknessxd@users.noreply.github.com>
2022-06-28 14:00:31 -0300

    desafio 1

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 function msg1(){
-    let m1 = alert("Botao 1")
+    let m1 = window.alert("Você clicou no 1° Botão")
+}
+
+function msg2(){
+    let m1 = window.alert("Você clicou no 2° Botão")
+}
+
+function msg3(){
+    let m1 = window.alert("Você clicou no 3° Botão")
 }
