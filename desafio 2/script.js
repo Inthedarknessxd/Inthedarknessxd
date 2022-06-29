55c11ff6e819a05bf4acfca61474f6a8e62be05f
Inthedarknessxd <108297974+Inthedarknessxd@users.noreply.github.com>
2022-06-29 16:32:41 -0300

    desafios

--- desafio 2/script.js
+++ desafio 2/script.js
@@ -1,5 +1,5 @@
 function desafio(){
     let nome = window.prompt("Qual seu nome?")
     let idade = window.prompt(`Olá ${nome} Quantos anos você tem?`)
-    let res = window.prompt(`Acabei de conhcer ${nome}, que tem ${idade} anos de idade! `)
+    let res = window.alert(`Acabei de conhcer ${nome}, que tem ${idade} anos de idade! `)
 }
