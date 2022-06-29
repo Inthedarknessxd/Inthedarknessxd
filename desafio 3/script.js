248ffd45b482b2227e533f9298fe1d2f63548bd9
Inthedarknessxd <108297974+Inthedarknessxd@users.noreply.github.com>
2022-06-29 16:55:30 -0300

    desafios

---
+++ desafio 3/script.js
@@ -0,0 +1,6 @@
+function aes() {
+let  num = window.prompt("Digite um Numero")
+let numMenos = num - 1
+let numMais = num+1
+let rusultado = window.alert(`Antes de ${num}, temos o numero ${numMenos} /nDepois de ${num},temos o numero ${numMais}`)
+}
